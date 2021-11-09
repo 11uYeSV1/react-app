c0f5c51dacc5f4f19665aa28069bacc5ad25763d
11uYeSV1 <levakkies@yandex.ru>
2021-11-09 12:11:57 +0300

    initial commit

---
+++ src/components/Faq/Form.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import styles from './faq.module.scss'
+
+export class Form extends Component {
+  state = {
+    title: null,
+    about: null,
+  };
+
+  handleChange = (e) => {
+    if (e.target.value) {
+      this.setState({ [e.target.name]: e.target.value });
+    }
+  };
+
+  handleSubmitForm = (e) => {
+    e.preventDefault();
+    const { title, about } = this.state;
+
+    if (title && about) {
+      this.props.addArticle({ title, about });
+      this.setState({
+        title: '',
+        about: '',
+      })
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmitForm}>
+          <fieldset>
+            <legend>Форма</legend>
+            <label className={styles.label}>
+              <input
+                onChange={this.handleChange}
+                value={this.state.title}
+                type="text"
+                name="title"
+                placeholder="Добавьте заголовок"
+              />
+            </label>
+            <label className={styles.label}>
+              <input
+                onChange={this.handleChange}
+                value={this.state.about}
+                type="text"
+                name="about"
+                placeholder="Добавьте текст"
+              />
+            </label>
+            <button type="submit">Добавить</button>
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
